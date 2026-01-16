3c38ebfc7ba5cdc0932edc2eacf90bb72105a031
dambrosiosalvatore357-lgtm <dambrosiosalvatore357@gmail.com>
2026-01-16 16:26:14 +0100

    Update config.js
    Napoli bot

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.nomebot = 'Napoli-Bot';
 global.packname = 'Napoli-Bot';
 global.vs = '8.8';
 global.collab = 'Demon Slayer';
-global.wm = global.Napoli-Bot';
+global.wm = global.Napoli-Bot;
 global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
