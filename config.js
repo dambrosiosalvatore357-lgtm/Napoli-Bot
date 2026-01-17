d575ca3ee5428d9c92d18a4c947272e50f70d8eb
dambrosiosalvatore357-lgtm <dambrosiosalvatore357@gmail.com>
2026-01-17 10:56:08 +0100

    Update config.js
    Napoli bot

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 global.owner = [
   ['393336413960', '💙Salvatore💙', true],
   ['19184025124', 'Simone', true],
-  ['××××××××××××,],
+  ['xxxxxxxxxxxx,],
   ['393773800915', ',Andrea😎👑 ',true],
   ['xxxxxxxxxxxx'],
   ['xxxxxxxxxxxx'],
